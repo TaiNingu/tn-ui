70fa613187049f6ce1b546ddb3b99adeb5889ab3
tngu <gtn.jingfeng@163.com>
2021-01-14 15:53:26 +0800

    按需引入

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
+  ],
+  plugins: [
+    [
+      "import",
+      {
+        "libraryName": "tnui", // 组件名
+        "libraryDirectory": "packages" // 组件源码包名
+      }
+    ]
   ]
 }
--- packages/index.js
+++ packages/index.js
@@ -21,11 +21,16 @@ const install = function (Vue) {
 if (typeof window !== 'undefined' && window.Vue) {
   install(window.Vue)
 }
- 
-export default {
-  // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
+export {
+  // 按需引入
   install,
   // 以下是具体的组件列表
   XButton,
   XInput
+}
+export default {
+  // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
+  install,
+  // 以下是具体的组件列表
+  ...components
 }
